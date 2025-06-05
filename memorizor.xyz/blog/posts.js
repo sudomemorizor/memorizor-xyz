1d5b1b2225b6257831595a116bdcd2a6ece10862
Zero S. <83229877+sudomemorizor@users.noreply.github.com>
2025-06-05 10:51:44 -0400

    Add files via upload

---
+++ memorizor.xyz/blog/posts.js
@@ -0,0 +1,16 @@
+const blogPosts = [
+  {
+    title: "New Track out on Soundcloud and Bandcamp",
+    date: "2025-06-05",
+    url: "post-2.html",
+    excerpt: "Listen to my new track out on Soundcloud and Bandcamp + Stems.",
+    tags: ["updates", "music"]
+  },
+  {
+    title: "Welcome to My Blog",
+    date: "2025-06-02",
+    url: "post-1.html",
+    excerpt: "This is my first blog post. Here's what to expect in the future.",
+    tags: ["personal", "intro"]
+  }
+];
